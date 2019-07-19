34230a9668d541fab9a565c0089e1d7d5571844b
Yann Kozon <contact@yannkozon.com>
2019-07-19 02:38:13 +0200

    Support de Gutenberg

--- remove-h1-wp-editor.php
+++ remove-h1-wp-editor.php
@@ -3,7 +3,7 @@ defined('ABSPATH') or die();
 
 /*
 MU Plugin: remove-h1-wp-editor
-Description: Remove H1 in wp editor
+Description: Remove H1 in wp editor / gutenberg
 Author: Yann Kozon
 Author URI: https://www.yannkozon.com
 */
@@ -13,3 +13,15 @@ function mu_remove_h1_wp_editor( $init ) {
     return $init;
 }
 add_filter( 'tiny_mce_before_init', 'mu_remove_h1_wp_editor' );
+
+function mu_remove_h1_gutenberg_editor() {
+	echo '<style>
+	#editor .components-button.components-toolbar__control[data-subscript="1"] {
+		width: 3px;
+		padding: 3px 0;
+		pointer-events: none;
+		visibility: hidden;
+	}
+	</style>';
+}
+add_action( 'admin_head', 'mu_remove_h1_gutenberg_editor' );
